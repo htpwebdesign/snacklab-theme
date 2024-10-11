03f857f661984d03d06bdb98f6ae03802d2e1074
Gustavo <gustavoyamamoto@gmail.com>
2024-10-11 16:27:13 -0700

    Added featured image and gallery to "Catering" section

--- page-catering.php
+++ page-catering.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -14,25 +15,38 @@
 
 get_header();
 ?>
+<main id="primary" class="site-main">
+	<?php
+	while (have_posts()) :
+		the_post();
+	?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+			</header><!-- .entry-header -->
 
-	<main id="primary" class="site-main">
+			<?php snacklab_theme_post_thumbnail(); ?>
+			<div class="entry-content">
+				<?php
+				the_content();
+				?>
+				<div class="catering-gallery">
+					<?php
+					$images = get_field('catering_gallery');
+					if ($images) {
+						foreach ($images as $image) {
+							echo '<img src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
+						}
+					}
+					?>
+				</div>
+			</div><!-- .entry-content -->
+		</article><!-- #post-<?php the_ID(); ?> -->
+	<?php
+	endwhile; // End of the loop.
+	?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
