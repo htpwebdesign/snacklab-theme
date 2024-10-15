a0d71a7f93c1c9b5fb5bd1c94f424d0cc5b94c0a
Gustavo <gustavoyamamoto@gmail.com>
2024-10-15 11:40:38 -0700

    deleted widget

--- functions.php
+++ functions.php
@@ -125,21 +125,21 @@ add_action('after_setup_theme', 'snacklab_theme_content_width', 0);
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function snacklab_theme_widgets_init()
-{
-	register_sidebar(
-		array(
-			'name'          => esc_html__('Sidebar', 'snacklab-theme'),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__('Add widgets here.', 'snacklab-theme'),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action('widgets_init', 'snacklab_theme_widgets_init');
+// function snacklab_theme_widgets_init()
+// {
+// 	register_sidebar(
+// 		array(
+// 			'name'          => esc_html__('Sidebar', 'snacklab-theme'),
+// 			'id'            => 'sidebar-1',
+// 			'description'   => esc_html__('Add widgets here.', 'snacklab-theme'),
+// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 			'after_widget'  => '</section>',
+// 			'before_title'  => '<h2 class="widget-title">',
+// 			'after_title'   => '</h2>',
+// 		)
+// 	);
+// }
+// add_action('widgets_init', 'snacklab_theme_widgets_init');
 
 /**
  * Enqueue scripts and styles.
