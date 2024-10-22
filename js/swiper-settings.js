fa1eee9558839b31929d6ffc2e9f94559628bd7f
Edmarc Sapa <marcgsapa@gmail.com>
2024-10-22 15:04:33 -0700

    Styling Front Page

--- js/swiper-settings.js
+++ js/swiper-settings.js
@@ -13,8 +13,8 @@ const swiper = new Swiper(".swiper", {
   spaceBetween: 10,
   breakpoints: {
     800: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
   },
 });
