3476ba6988b271f232f0b3b7798ed3694f77666e
Haw Haw Tan <113495100+HawHawTan@users.noreply.github.com>
2024-10-28 13:47:19 -0700

    pull acf content to the about page

--- page-career.php
+++ page-career.php
@@ -14,7 +14,7 @@ get_header();
     <?php
     while (have_posts()) :
         the_post();
-
+         // help is it okay to use get template part 
         get_template_part('template-parts/content', 'page');
         $args = array(
             'post_type' => 'snacklab-careers', // Custom post type
@@ -24,31 +24,29 @@ get_header();
         if ($careers_query->have_posts()) :
             while ($careers_query->have_posts()) : $careers_query->the_post();
     ?>
+        <div class="accordion">
+            <!-- Accordion button (click to expand/collapse content) -->
+            <button class="accordion-btn">
+                <h2><?php the_title(); ?></h2>
+            </button>
 
-                <div class="accordion">
-                    <!-- Accordion button (click to expand/collapse content) -->
-                    <button class="accordion-btn">
-                        <h2><?php the_title(); ?></h2>
-                    </button>
-
-                    <!-- Accordion content (initially hidden) -->
-                    <div class="accordion-content">
-                        <div class="entry-content">
-                            <?php if (function_exists('get_field')) : ?>
-                                <?php if (get_field('job_description')) : ?>
-                                    <p><?php the_field('job_description'); ?></p>
-                                <?php endif; ?>
-                            <?php endif; ?>
-                        </div>
-                    </div>
+            <!-- Accordion content (initially hidden) -->
+            <div class="accordion-content">
+                <div class="entry-content">
+                    <?php if (function_exists('get_field')) : ?>
+                        <?php if (get_field('job_description')) : ?>
+                            <p><?php the_field('job_description'); ?></p>
+                        <?php endif; ?>
+                    <?php endif; ?>
                 </div>
-
+            </div>
+        </div>
     <?php
-            endwhile;
-            wp_reset_postdata(); // Restore global post data
-            echo do_shortcode("[gravityform id=3 title=true]");
-        else :
-            echo '<p>No careers found.</p>';
+        endwhile;
+        wp_reset_postdata(); // Restore global post data
+        echo do_shortcode("[gravityform id=3 title=true]");
+    else :
+        echo '<p>No careers found.</p>';
         endif;
     endwhile; // End of the loop.
     ?>
