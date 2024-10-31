6a3eaf0f08cd93ff0658454ddea96ff7a638491e
Haw Haw Tan <113495100+HawHawTan@users.noreply.github.com>
2024-10-31 00:45:28 -0700

    added a video to the dashboard

--- functions.php
+++ functions.php
@@ -266,7 +266,7 @@ remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30
 function add_custom_dashboard_widget() {
     wp_add_dashboard_widget(
         'custom_help_widget', // Widget ID
-        'How to Edit Posts',  // Widget title
+        'If you need help! Check these vidoes!',  // Widget title
         'custom_dashboard_help' // Callback function
     );
 }
@@ -278,19 +278,34 @@ function add_custom_dashboard_widget() {
 // }
 
 function custom_dashboard_help() {
-    $video_url = 'https://snacklab.bcitwebdeveloper.ca/wp-content/uploads/2024/10/snacklabloading.webp'; 
+    $storelocator_url  = "http://localhost/wordpress-snackLab/wp-content/uploads/2024/10/how-to-use-storeLocator.mp4";
+    $job_post_url  = "http://localhost/wordpress-snackLab/wp-content/uploads/2024/10/Add-Job-Post.mp4";
+    $acf_url  = "http://localhost/wordpress-snackLab/wp-content/uploads/2024/10/Where-to-edit-content.mp4";
+    $product_url  = "http://localhost/wordpress-snackLab/wp-content/uploads/2024/10/Adding-product.mp4";
 
-    echo '<p>Watch the video below to learn how to edit posts:</p>';
+    echo '<p>Watch the video below to learn how to edit job posts:</p>';
     echo '<video width="560" height="315" controls>
-            <source src="' . esc_url($video_url) . '" type="video/mp4">
+            <source src="' . esc_url($job_post_url) . '" type="video/mp4">
             Your browser does not support the video tag.
           </video>';
 
-		echo '<p>Watch the video below to learn how to edit posts:</p>';
-		echo '<video width="560" height="315" controls>
-				<source src="' . esc_url($video_url) . '" type="video/mp4">
-				Your browser does not support the video tag.
-			</video>';
+	echo '<p>Watch the video below to learn how to edit content on each pages:</p>';
+	echo '<video width="560" height="315" controls>
+			<source src="' . esc_url($acf_url) . '" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>';
+
+	echo '<p>Watch the video below to learn how to add products:</p>';
+	echo '<video width="560" height="315" controls>
+			<source src="' . esc_url($product_url) . '" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>';
+
+	echo '<p>Watch the video below to learn how to add new location:</p>';
+	echo '<video width="560" height="315" controls>
+			<source src="' . esc_url($storelocator_url) . '" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>';
 }
 
 add_action('wp_dashboard_setup', 'add_custom_dashboard_widget');
